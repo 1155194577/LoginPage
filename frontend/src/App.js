96a23666a92021b3ecf188087458aa37fd64f1ff
1155194577 <1155194577@link.cuhk.edu.hk>
2024-05-20 16:15:35 +0800

    Added Scrolling table in the database route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { QueryClient,QueryClientProvider } from 'react-query'
 import Logincomponent from './Logincomponent'
 import Navbar from './Navbar';
 import Database from './database';
+import DataFormatting from './database';
 const queryClient = new QueryClient()  //
 export const Context = React.createContext();
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <Context.Provider value={[IsLoggedin,SetIsLoggedin,IsCheckingDataBase,SetIsCheckingDataBase]}>
     <QueryClientProvider client={queryClient}>   
     <Navbar/>
-     { IsLoggedin ?  <div>Logged in!</div> :  IsCheckingDataBase ?<Database/>:<Logincomponent/> }
+     { IsLoggedin ?  <div>Logged in!</div> :  IsCheckingDataBase ?<DataFormatting/>:<Logincomponent/> }
     </QueryClientProvider>
     </Context.Provider>
   )
--- frontend/src/database.js
+++ frontend/src/database.js
@@ -1,18 +1,16 @@
-import React from 'react'
 import { useQuery } from 'react-query';
 import axios from 'axios';
-function DataFormatting(arr){
-    const kvpair = arr.reduce(
-      (obj,item) =>  {
-        obj[item.user_name]=item.user_password;
-        return obj; 
-      },[]
-    );
-      return kvpair; 
-  }
-  
-function Database() {
-    const {isLoading,error,data} = useQuery('datakey',()=>{
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+export default function DataFormatting() {
+   const {isLoading,error,data} = useQuery('datakey',()=>{
         return axios.get("http://localhost:5001/api")
       });
       console.log(isLoading)
@@ -24,19 +22,35 @@ function Database() {
       }
 
       console.log(data);
-      const arr = data["data"];
-      const kvpair = DataFormatting(arr);
-      console.log(kvpair);
+      const rows = data["data"];
+// const rows = [ { key: 7, user_name: 'admin', user_password: 'admin' } ]
   return (
-    <div>
-       { arr.map( 
-            (item) => (<div>{item.user_id},{item.user_name},{item.user_password}</div>)
-        )}
-    </div>
-  )
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>User ID</TableCell>
+            <TableCell align="right">User Name</TableCell>
+            <TableCell align="right">User Password</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.user_key}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.user_id}
+              </TableCell>
+              <TableCell align="right">{row.user_name}</TableCell>
+              <TableCell align="right">{row.user_password}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+
+
 }
-
-export default Database
-
-
-
