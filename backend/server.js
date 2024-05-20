bacfcabfd4106c17c3c7d1b3605cfaf9fe03f480
1155194577 <1155194577@link.cuhk.edu.hk>
2024-05-20 10:24:17 +0800

    Implemented a user validation in backend , as well as allowing server to sent correctness of username and password to frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const  mysql =require("mysql2")
+var crypto = require('crypto')
+const { error } = require("console")
+const { resolveSoa } = require("dns")
+
 
 const url = "http://localhost:3001";
 app.use(express.json())
@@ -9,12 +13,41 @@ app.use(express.json())
 app.use(
     cors({
         origin : url,
-    })
+    }) 
 )
 
+
+function authentication(username,password,res) {
+    con.connect(function(err) {
+        if (err) {
+            throw error;
+        };
+        const query1 = `SELECT * FROM table1 WHERE user_name = '${username}' AND user_password = '${password}' `
+        con.query(query1, function (err, result, fields) {
+              if (err) {
+                    res.status(500).json({error:"error in db"})
+                    return; 
+              };
+
+              if (!result.length){
+                    console.log("Username/Password not found!")
+                    res.status(200).json({ Correct: false })
+                    return; 
+                }
+              else {
+                console.log("found!")
+                console.log(result);
+                res.status(200).json({ Correct: true })
+                return; 
+              }  
+            });
+              }); 
+    }
+
 app.post("/postdata",(req,res)=>{
-    console.log(req.body)   
-    res.send("posted to server"); 
+    const up_pair = req.body
+    console.log(up_pair)
+    authentication(up_pair["Username"],up_pair["Userpassword"],res)
 })
 
 var con = mysql.createConnection(
@@ -26,14 +59,18 @@ var con = mysql.createConnection(
     }
     ); 
 
+// this code shows all username-password at frontend 
 con.connect(function(err) {
         if (err) throw err;
-        con.query("SELECT * FROM table1", function (err, result, fields) {
+        const query1 = "SELECT * FROM table1"
+        con.query(query1, function (err, result, fields) {
           if (err) throw err;
           console.log(result);
           app.get("/api",(req,res)=>{res.json(result)})
         });
           });
+
+
 
 //app.get("/api",(req,res)=>{
  //   res.json(
@@ -46,4 +83,3 @@ app.listen(5001,()=>{
     console.log("listening on 5001!"); 
 })
 
-// POST /login gets urlencoded bodies
